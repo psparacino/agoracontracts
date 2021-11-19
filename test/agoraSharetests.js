02d620994fe37a983fec3bf3e6dad1d1c93e6dac
godtide <ayoadebabatide@gmail.com>
2021-11-19 06:01:41 +0100

    buytest

--- test/agoraSharetests.js
+++ test/agoraSharetests.js
@@ -9,6 +9,8 @@ const { ethers } = require("hardhat");
 
      
       let NftIdNext;
+      let sharedIdNext;
+      let accounts;
       const txnAmt = "250000000000000000000000";
       const walletAddr = process.env.WALLET_ADDR;
 
@@ -42,7 +44,7 @@ const { ethers } = require("hardhat");
     let result = await AgoraContract.shareAgoraNft(NftIdNext, txnAmt)
      // wait until the transaction is mined
    await result.wait();
-   let sharedIdNext = await AgoraContract.sharedId();
+   sharedIdNext = await AgoraContract.sharedId();
    expect(sharedIdNext).to.be.gt(sharedId);
   //  expect(NftId == NftIdNext).to.be.false;
   })
@@ -57,18 +59,59 @@ const { ethers } = require("hardhat");
   xit("The new owner of nftId should be contract address")
 
 
-  ------------------------------------------------------------------------------------------------------
+//   ---------------------------------------------------------------------------------- buyShares
  
-  it('The amount of token should reduce after buying', async () => {
+  it('The balance of token sharer should reduce after buying', async () => {
     // let zerobalance = 0;
-    // let balance = await   AgoraShareContract.balanceOf(walletAddr);
-
-  function buyShares(uint16 _sharedId, uint16 amount) 
+    let amountToBuy = 1;
+     
+    let balance = await   AgoraShareContract.balanceOf(walletAddr);
     console.log("sharedId", balance)
-    expect(balance).to.be.gt(zerobalance);
+
+    let boughtShares = await AgoraShareContract.buyShares(sharedIdNext, amountToBuy).connect(account[2]);
+
+    boughtShares.wait(1);
+
+    let newBalance =  AgoraShareContract.balanceOf(walletAddr);
+    
+    console.log("newBalance:", newBalance)
+    expect(newBalance).to.be.lt(balance);
 
  })
 
 
+ it('The balance of token buyer should increase after buying', async () => {
+    // let zerobalance = 0;
+    let amountToBuy = 1;
+     
+    let balance = await   AgoraShareContract.balanceOf(accounts[2]);
+    console.log("sharedId", balance)
   
+    let boughtShares = await AgoraShareContract.buyShares(sharedIdNext, amountToBuy).connect(account[2]);
+
+    boughtShares.wait(1);
+
+    let newBalance =  AgoraShareContract.balanceOf(accounts[2]);
+    
+    console.log("newBalance:", newBalance)
+    expect(newBalance).to.be.gt(balance);
+
+ })
+
+
+ it('The investors list at index 0 should be equal to msg.sender or token buyer should increase after buying', async () => {
+    let investorlist =   AgoraShareContract.investors[sharedIdNext];
+
+    let i = 0;
+
+    while (investorlist.length > 0){
+    console.log("sharedId", investorlist[i])
+    }
+    expect((investorlist[0] == accounts[2]));
+ })
+
+
+//  -------------------------------------------------------------------------------------------------- redeem
+
+
 })
